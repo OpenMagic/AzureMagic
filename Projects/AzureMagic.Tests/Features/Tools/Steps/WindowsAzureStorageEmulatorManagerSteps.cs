352049dc5b2ed9ab2ec0c0bd53105d29d2101d15
Tim Murphy <tim@26tp.com>
2014-07-13 13:55:37 +1000

    Updated WindowsAzureStorageEmulatorManager to use Windows Azure Storage Emulator v3.2.

---
+++ Projects/AzureMagic.Tests/Features/Tools/Steps/WindowsAzureStorageEmulatorManagerSteps.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading;
+using AzureMagic.Tools;
+using FluentAssertions;
+using TechTalk.SpecFlow;
+
+namespace AzureMagic.Tests.Features.Tools.Steps
+{
+    [Binding]
+    public class WindowsAzureStorageEmulatorManagerSteps
+    {
+        [Given(@"the Windows Azure Storage Emulator is not running")]
+        public void GivenTheAzureStorageEmulatorIsNotRunning()
+        {
+            WindowAzureStorageEmulatorManager.StopEmulator();
+            Sleep();
+        }
+
+        [When(@"StartEmulator is called")]
+        public void WhenStartEmulatorIsCalled()
+        {
+            WindowAzureStorageEmulatorManager.StartEmulator();
+            Sleep();
+        }
+
+        [Then(@"IsEmulatorRunning returns true")]
+        public void ThenIsEmulatorRunningReturnsTrue()
+        {
+            WindowAzureStorageEmulatorManager.IsEmulatorRunning().Should().BeTrue();
+        }
+
+        [Given(@"the Windows Azure Storage Emulator is running")]
+        public void GivenTheAzureStorageEmulatorIsRunning()
+        {
+            WindowAzureStorageEmulatorManager.StartEmulator();
+            Sleep();
+        }
+
+        [When(@"StopEmulator is called")]
+        public void WhenStopEmulatorIsCalled()
+        {
+            WindowAzureStorageEmulatorManager.StopEmulator();
+            Sleep();
+        }
+
+        [Then(@"IsEmulatorRunning returns false")]
+        public void ThenIsEmulatorRunningReturnsFalse()
+        {
+            WindowAzureStorageEmulatorManager.IsEmulatorRunning().Should().BeFalse();
+        }
+
+        private static void Sleep()
+        {
+            // A little sleep after each Given & When ensures the Then will pass when all 4 scenarios are run.
+            Thread.Sleep(75);
+        }
+    }
+}
---
+++ Projects/AzureMagic.Tests/Features/Tools/WindowsAzureStorageEmulatorManager.feature
@@ -0,0 +1,23 @@
+﻿# These tests individual pass but in same run all 4 passing is rare. Hence why they are ignored.
+
+Feature: WindowsAzureStorageEmulatorManager
+
+Scenario: Start emulator when it is not running
+	Given the Windows Azure Storage Emulator is not running
+	When StartEmulator is called
+	Then IsEmulatorRunning returns true
+
+Scenario: Start emulator when it is running
+	Given the Windows Azure Storage Emulator is running
+	When StartEmulator is called
+	Then IsEmulatorRunning returns true
+
+Scenario: Stop emulator when it is not running
+	Given the Windows Azure Storage Emulator is not running
+	When StopEmulator is called
+	Then IsEmulatorRunning returns false
+
+Scenario: Stop emulator when it is running
+	Given the Windows Azure Storage Emulator is running
+	When StopEmulator is called
+	Then IsEmulatorRunning returns false

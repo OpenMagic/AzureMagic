96d895c92b329f7479c4ae473306abbc93623bc1
Tim Murphy <tim@26tp.com>
2014-07-16 05:50:59 +1000

    Added specification for AzureQueryExtensions.ExecuteAsync<TTableEntity, TEntity>(this IQueryable<TTableEntity> query).

---
+++ Projects/AzureMagic/AzureQueryExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace AzureMagic
+{
+    public static class AzureQueryExtensions
+    {
+        public static Task<IEnumerable<TEntity>> ExecuteAsync<TTableEntity, TEntity>(this IQueryable<TTableEntity> query) where TTableEntity : ITableEntity
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

a608428e5f1092617f652897d8757f572845a781
Tim Murphy <tim@26tp.com>
2014-07-20 16:52:58 +1000

    Added AzureTableStorage.ValidateTableName(tableName).

---
+++ Projects/AzureMagic.Tests/Features/Tables/AzureTableStorage.ValidateTableName.feature
@@ -0,0 +1,11 @@
+﻿Feature: AzureTableStorage.ValidateTableName
+
+Scenario: ValidateTableName when tableName is valid
+	Given a valid tableName
+	When ValidateTableName is called
+	Then an exception is not thrown
+
+Scenario: ValidateTableName when tableName is invalid
+	Given a invalid tableName
+	When ValidateTableName is called
+	Then an exception is thrown
---
+++ Projects/AzureMagic.Tests/Features/Tables/Steps/AzureTableStorageValidateTableNameSteps.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using AzureMagic.Tables;
+using FluentAssertions;
+using TechTalk.SpecFlow;
+
+namespace AzureMagic.Tests.Features.Tables.Steps
+{
+    [Binding]
+    public class AzureTableStorageValidateTableNameSteps
+    {
+        private string TableName;
+        private Exception Exception;
+
+        [Given(@"a valid tableName")]
+        public void GivenAValidTableName()
+        {
+            GivenTableName("abc");
+        }
+
+        private void GivenTableName(string tableName)
+        {
+            TableName = tableName;
+        }
+
+        [Given(@"a invalid tableName")]
+        public void GivenAInvalidTableName()
+        {
+            TableName = "a".PadRight(64, 'b');
+        }
+
+        [When(@"ValidateTableName is called")]
+        public void WhenValidateTableNameIsCalled()
+        {
+            try
+            {
+                TableName.ValidateTableName();
+            }
+            catch (Exception exception)
+            {
+                Exception = exception;
+            }
+        }
+
+        [Then(@"an exception is not thrown")]
+        public void ThenAnExceptionIsNotThrown()
+        {
+            Exception.Should().BeNull();
+        }
+
+        [Then(@"an exception is thrown")]
+        public void ThenAnExceptionIsThrown()
+        {
+            Exception.Should().BeOfType<ValidationException>();
+        }
+    }
+}
